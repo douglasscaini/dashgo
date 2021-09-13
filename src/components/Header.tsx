80a778e7dd79a4cf67a6fa17afb8cf431171c5d5
Douglas Scaini <douglas.scaini@unoesc.edu.br>
2021-09-13 14:31:46 -0300

    feat: Create Sidebar

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import { RiSearchLine, RiNotificationLine, RiUserAddLine } from "react-icons/ri"
 
 export function Header() {
   return (
-    <Flex as="header" w="100%" maxWidth={1400} h="20" mx="auto" mt="4" px="6" align="center">
+    <Flex as="header" w="100%" maxWidth={1480} h="20" mx="auto" mt="4" px="6" align="center">
       <Text fontSize="3xl" fontWeight="bold" letterSpacing="tight" w="64">
         dashgo
         <Text as="span" ml="1" color="pink.500">
