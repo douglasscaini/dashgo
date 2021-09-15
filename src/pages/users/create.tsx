651e4251549eeb40b8ae3a310b399e177e877a78
Douglas Scaini <douglas.scaini@unoesc.edu.br>
2021-09-15 13:24:20 -0300

    chore: Link Active

--- src/pages/users/create.tsx
+++ src/pages/users/create.tsx
@@ -1,5 +1,5 @@
 import { Box, Flex, Heading, Button, Divider, VStack, SimpleGrid, HStack } from "@chakra-ui/react";
-
+import Link from "next/link";
 import { Input } from "../../components/Form/Input";
 import { Header } from "../../components/Header";
 import { SideBar } from "../../components/Sidebar";
@@ -37,7 +37,11 @@ export default function CreateUser() {
 
           <Flex mt="8" justify="flex-end">
             <HStack spacing="4">
-              <Button colorScheme="whiteAlpha">Cancelar</Button>
+              <Link href="/users" passHref>
+                <Button as="a" colorScheme="whiteAlpha">
+                  Cancelar
+                </Button>
+              </Link>
               <Button colorScheme="pink">Salvar</Button>
             </HStack>
           </Flex>
