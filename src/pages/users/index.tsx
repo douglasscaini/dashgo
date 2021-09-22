68d474a3e5748d7dcf0871806fa1d341bf1d1fd4
Douglas Scaini <douglas.scaini@unoesc.edu.br>
2021-09-22 11:30:45 -0300

    chore: Create Hook Users

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -17,39 +17,15 @@ import {
 } from "@chakra-ui/react";
 
 import Link from "next/link";
-import { useQuery } from "react-query";
 import { Header } from "../../components/Header";
 import { Pagination } from "../../components/Pagination";
 import { SideBar } from "../../components/Sidebar";
-import { api } from "../../services/api";
+import { useUser } from "../../services/hooks/useUsers";
 
 import { RiAddLine } from "react-icons/ri";
 
 export default function UserList() {
-  const { data, isLoading, isFetching, error } = useQuery(
-    "users",
-    async () => {
-      const { data } = await api.get("users");
-
-      const users = data.users.map((user) => {
-        return {
-          id: user.id,
-          name: user.name,
-          email: user.email,
-          createdAt: new Date(user.createdAt).toLocaleDateString("pt-BR", {
-            day: "2-digit",
-            month: "long",
-            year: "numeric",
-          }),
-        };
-      });
-
-      return users;
-    },
-    {
-      staleTime: 1000 * 5, // 5 seconds
-    }
-  );
+  const { data, isLoading, isFetching, error } = useUser();
 
   const isWideVersion = useBreakpointValue({
     base: false,
